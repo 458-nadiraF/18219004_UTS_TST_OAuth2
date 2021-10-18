e73e1b3895f4c562a04c01d3d2111713845e1623
458-nadiraF <nadira.fwzyya@gmail.com>
2021-10-18 08:15:45 +0700

    commit

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-# main.py
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
-import uvicorn
+app = FastAPI()
 
-if __name__ == "__main__":
-    uvicorn.run("app.api:app", host="0.0.0.0", port=8081, reload=True)
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    data = {
+        "page": "Home page"
+    }
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
+
+
+@app.get("/page/{page_name}", response_class=HTMLResponse)
+async def page(request: Request, page_name: str):
+    data = {
+        "page": page_name
+    }
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
